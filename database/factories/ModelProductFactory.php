14d70dbe32d792ff05559ecb600763b51c1b7667
Unknown <zMrSnow@icloud.com>
2017-12-02 12:37:11 +0100

    - migrations update - factoryes + seeds

--- database/factories/ModelProductFactory.php
+++ database/factories/ModelProductFactory.php
@@ -4,6 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        "name" => $faker->word,
+        "detail" => $faker->paragraph,
+        "price" => $faker->numberBetween(1, 100),
+        "stock" => $faker->randomDigit
     ];
 });
--- database/migrations/2017_12_02_091708_create_products_table.php
+++ database/migrations/2017_12_02_091708_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string("name", 255);
+            $table->text("detail");
+            $table->integer("price");
+            $table->integer("stock");
             $table->timestamps();
         });
     }
