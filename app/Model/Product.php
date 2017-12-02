d63c9247b0de658d5d857157bd03a90327523a8a
Unknown <zMrSnow@icloud.com>
2017-12-02 13:31:54 +0100

    - added resource for tranformation product - api/product - api/product/{id}

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+
+    protected $hidden = ["created_at", "updated_at"];
+
+    public function reviews() {
+        return $this->hasMany(Review::class);
+    }
 }
